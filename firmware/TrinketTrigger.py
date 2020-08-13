d2cdb59ef707f8bc246608ac3ae36f2dde7a89a0
Tod E. Kurt <tod@todbot.com>
2020-08-13 12:51:52 -0700

    added rec button support

--- firmware/TrinketTrigger.py
+++ firmware/TrinketTrigger.py
@@ -1,4 +1,4 @@
-# Trinket Trigger 1.0
+# Trinket Trigger v2
 # todbot
 
 import time
@@ -7,9 +7,10 @@ import digitalio
 import touchio
 import analogio
 import adafruit_dotstar as dotstar
+from adafruit_debouncer import Debouncer
 
 # On-board DotStar for boards including Gemma, Trinket, and ItsyBitsy
-dots = dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=0.5)
+dots = dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=0.5, auto_write=False)
 
 touchA= touchio.TouchIn(board.D3)
 touchB= touchio.TouchIn(board.D4)
@@ -18,6 +19,10 @@ gate = digitalio.DigitalInOut(board.D2)
 gate.direction = digitalio.Direction.OUTPUT
 gate.value = False
 
+rswitch = digitalio.DigitalInOut(board.D0)
+rswitch.direction = digitalio.Direction.INPUT
+rswitch.pull = digitalio.Pull.UP
+rec_switch = Debouncer( rswitch )
 
 t0 = time.monotonic() # set start time
 
@@ -39,22 +44,61 @@ def map_range(x, in_min, in_max, out_min, out_max):
 
 touchA_min = touchA.raw_value
 
+recording = False
+record_start = 0
+record_events = []
+last_time = 0
+delta_t = 0.05
+playing_idx = 0
+playing = False
+
 while True:
+    dots.show()
+    rec_switch.update()
 
-    v = map_range(touchA.raw_value, touchA_min,touchA_min*2, 0,255)
-    print("AA: ", touchA.threshold, touchA.raw_value - touchA.threshold, int(v))
+    if not playing:
+        touchA_val = map_range(touchA.raw_value, touchA_min,touchA_min*2, 0,255)
+        touchB_val = touchB.value
 
-    # set red part of dotstar LED to indicate A touch amount
-    dots[0] = (int(v),0,0)
+    now = time.monotonic()
+    if (now - last_time) > delta_t:
+        last_time = now
+        if recording:
+            record_events.append( ((time.monotonic()-record_start), touchA_val, touchB_val) )
+        else:
+            if playing:
+                (t, touchA_val, touchB_val) = record_events[playing_idx]
+                playing_idx = (playing_idx + 1) % (len(record_events))
+                #print("i:",playing_idx,"t:",t,"tA:",touchA_val,"tB:",touchB_val)
+
+    #    print("AA: ", touchA.threshold, touchA.raw_value - touchA.threshold, int(v))
+    
+
+    # Output touch button state as red & blue parts of dotstar LED
+    # red part of dotstar LED indicates touch A amount
+    # blue part of dostart LED indicates touch B amount
+    dots[0] = (int(touchA_val), 0, int(touchB_val*255))
 
     # do the output
-    dac.value = int(v * 256)
-    gate.value = touchB.value
+    dac.value = int(touchA_val * 256)
+    gate.value = touchB_val
     
-    if touchA.value:
-        pass
 
-    if touchB.value:
-        # set blue part of doststar LED to indicate B touch
-        dots[0] = (0,0,255)
-
+    if rec_switch.fell:
+        dots[0] = (0,255,0)
+        playing = False
+        recording = True
+        record_start = time.monotonic()
+        record_events = []
+        last_time = record_start
+        
+    if rec_switch.rose:
+        dots[0] = (255,255,255)
+        recording = False
+        record_events.append( (time.monotonic()-record_start, 0, 0 ) )
+        print("events: ",len(record_events))
+        for e in record_events:
+            print( e )
+        if len(record_events) > 5:
+           playing = True  # FIXME
+        
